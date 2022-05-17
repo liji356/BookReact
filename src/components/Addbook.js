abb2e3ed95202aba89273ccedc7c2b44b530f805
liji356 <lijibthomas95@gmail.com>
2022-05-17 12:01:04 +0530

    Addbookbranch

---
+++ src/components/Addbook.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Addbook = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <dic className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="row g-3">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+     <label for="" className="form-label"> Book title</label>
+     <input type="text" className="form-control"/>
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+     <label for="" className="form-label">Author</label>
+     <input type="text" className="form-control"/>
+    </div>
+    <div className="col col-12 col-sm-16 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <label for="" className="form-label">Price</label>
+        <input type="text" className="form-control"/>
+    </div>
+    <div className="col col-12 col-sm-16 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <button className="btm btn-success">SUBMIT</button>
+    </div>
+</div>
+
+        </dic>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Addbook
